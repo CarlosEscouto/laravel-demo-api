b1fc3d423dbbebcbd2e962dc3d57db974ad22975
Carlos Henrique Escouto <carloshenriqueescouto@gmail.com>
2019-05-31 14:14:52 -0300

    Retirado pasta vendor

--- vendor/sarala-io/laravel-companion/config/sarala.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    /*
-     * League\Fractal\Serializer\JsonApiSerializer will use this value to
-     * as a prefix for generated links. Set to `null` to disable this.
-     */
-    'base_url' => env('API_URL', '/api'),
-];
